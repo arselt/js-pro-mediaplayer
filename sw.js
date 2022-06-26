411ed2f09bd7941081874186a98422544e3078ce
Arath Astorga <xarselt@gmail.com>
2022-06-26 12:37:34 -0500

    ✨👽 Added Service Workers

---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = "v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  // Other methods don't tend to be saved
+  if (request.method != "GET") {
+    return;
+  }
+
+  // Search through Cache
+  event.respondWith(cachedResponse(request));
+
+  //Then to network to look for changes
+  event.waitUntil(updateCache(request));
+});
+
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    "/",
+    "/index.html",
+    "/assets/index.js",
+    "/assets/MediaPlayer.js",
+    "/assets/plugins/AutoPlay.js",
+    "/assets/plugins/AutoPause.js",
+    "/assets/index.css",
+    "/assets/pinetimelapse.mp4",
+  ]);
+}
+
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  return cache.put(request, response);
+}
